7646e5320f33e73107b3e4d60d79cc2d3063cf95
Linus Torvalds <torvalds@linux-foundation.org>
2013-02-15 10:48:13 +0100

    added out of bounds test

---
+++ src/test/java/org/jpacman/test/framework/model/BoardBoundsTest.java
@@ -0,0 +1,64 @@
+package org.jpacman.test.framework.model;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.jpacman.framework.model.Board;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+/**
+ * @author rick This class tests the behaviour of positioning out of bounds
+ */
+@RunWith(Parameterized.class)
+public class BoardBoundsTest {
+	private final int startx, starty;
+	private final Board board;
+
+	/**
+	 * Create a new test case obtaining the tile from starting point (x, y) on a
+	 * board with the given width and height.
+	 * 
+	 * @param width
+	 *            Width of the board
+	 * @param height
+	 *            Height of the board
+	 * @param x
+	 *            X position
+	 * @param y
+	 *            Y position
+	 */
+	public BoardBoundsTest(int width, int height, int x, int y) {
+		startx = x;
+		starty = y;
+		board = new Board(width, height);
+	}
+
+	/**
+	 * The actual test case, we expect failure since we make a board that
+	 * doesn't contain the position.
+	 */
+	@Test(expected = ArrayIndexOutOfBoundsException.class)
+	public void testTileAtDirection() {
+		board.tileAt(startx, starty);
+	}
+
+	/**
+	 * List of (width, height, x, y) data points.
+	 * 
+	 * @return Test data to be fed to constructor.
+	 */
+	@Parameters
+	public static Collection<Object[]> data() {
+		Object[][] values = new Object[][] {
+				// Simply pick any point that's out of bounds
+				{ 1, 1, -1, -1 }, { 1, 1, -1, 0 }, { 1, 1, -1, 1 },
+				{ 1, 1, 0, -1 }, { 1, 1, 0, 1 }, { 1, 1, 1, -1 },
+				{ 1, 1, 1, 0 }, { 1, 1, 1, 1 }, { 1, 1, -1, 0 },
+				{ 0, 0, 0, 0 }, { 1, 0, 0, 0 }, { 0, 1, 0, 0 }, };
+		return Arrays.asList(values);
+	}
+
+}
