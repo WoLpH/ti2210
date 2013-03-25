d875e6f9ac43b3003d2d8a6c55a3cfd1f3cc3e29
WoLpH <Rick@Fawo.nl>
2013-03-25 13:39:07 +0100

    added redo button

--- src/main/java/org/jpacman/framework/ui/UndoablePacman.java
+++ src/main/java/org/jpacman/framework/ui/UndoablePacman.java
@@ -16,12 +16,6 @@ public class UndoablePacman extends MainUI {
 	 */
 	private static final long serialVersionUID = -1938866969221252775L;
 
-	@Override
-	public MainUI initialize() throws FactoryException {
-		withButtonPanel(new UndoButtonPanel());
-		return super.initialize();
-	}
-
 	/**
 	 * Main tarting point of the undoable pacman game.
 	 * 
@@ -32,6 +26,19 @@ public class UndoablePacman extends MainUI {
 	 */
 	public static void main(String[] args) throws FactoryException {
 		new UndoablePacman().main();
+	}
+
+	@Override
+	public MainUI initialize() throws FactoryException {
+		withButtonPanel(new UndoButtonPanel());
+		return super.initialize();
+	}
+
+	/**
+	 * Redo the last move.
+	 */
+	void redo() {
+
 	}
 
 	/**
