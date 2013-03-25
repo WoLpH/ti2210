48af24700bdee621cabdafddc7ab4773ee67018e
WoLpH <Rick@Fawo.nl>
2013-03-25 13:05:32 +0100

    added comment for undo story test

--- src/test/java/org/jpacman/test/framework/accept/UndoStoryTest.java
+++ src/test/java/org/jpacman/test/framework/accept/UndoStoryTest.java
@@ -4,8 +4,9 @@ import org.jpacman.framework.ui.MainUI;
 import org.jpacman.framework.ui.UndoablePacman;
 
 /**
- * @author Rick van Hattem <Rick.van.Hattem@Fawo.nl>
+ * Story test for the undoable pacman.
  * 
+ * @author Rick van Hattem <Rick.van.Hattem@Fawo.nl>
  */
 public class UndoStoryTest extends MovePlayerStoryTest {
 	private UndoablePacman pacman;
