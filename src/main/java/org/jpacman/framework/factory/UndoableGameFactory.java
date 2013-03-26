249bd14fd94668744f4bb4c331be1bc08082ea20
WoLpH <Rick@Fawo.nl>
2013-03-26 13:09:39 +0100

    added undoable game factory to return an undoable game

---
+++ src/main/java/org/jpacman/framework/factory/UndoableGameFactory.java
@@ -0,0 +1,20 @@
+package org.jpacman.framework.factory;
+
+import org.jpacman.framework.model.Game;
+import org.jpacman.framework.model.UndoableGame;
+
+/**
+ * Identical to {@link DefaultGameFactory} except that it returns an instance of
+ * {@link UndoableGame} instead of a Game instance.
+ * 
+ * @author Rick van Hattem <Rick.van.Hattem@Fawo.nl>
+ */
+public class UndoableGameFactory extends DefaultGameFactory {
+	private UndoableGame undoableGame;
+
+	@Override
+	public Game makeGame() {
+		this.undoableGame = new UndoableGame();
+		return this.undoableGame;
+	}
+}
