cb30d707c5e082820d38afdd05e5e429925d7532
WoLpH <Rick@Fawo.nl>
2013-04-01 01:33:07 +0200

    Added sprite subclass to enable class-specific actions.

--- src/main/java/org/jpacman/framework/model/UndoableGhost.java
+++ src/main/java/org/jpacman/framework/model/UndoableGhost.java
@@ -5,4 +5,13 @@ package org.jpacman.framework.model;
  * 
  */
 public class UndoableGhost extends Ghost implements IUndoableSprite {
+
+	/**
+	 * Create a new undoable ghost, not on any tile yet.
+	 */
+	public UndoableGhost() {
+		super();
+		this.mixin = new UndoableSpriteMixin(this);
+	}
+
 }
