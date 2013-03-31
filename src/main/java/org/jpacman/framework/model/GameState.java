ad56a847fce584304fb6e872658ed73434db24cf
WoLpH <Rick@Fawo.nl>
2013-04-01 01:33:07 +0200

    Adding class to store game state

---
+++ src/main/java/org/jpacman/framework/model/GameState.java
@@ -0,0 +1,15 @@
+package org.jpacman.framework.model;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map.Entry;
+
+import org.jpacman.framework.model.IBoardInspector.SpriteType;
+
+/**
+ * @author Rick van Hattem <Rick.van.Hattem@Fawo.nl>
+ * 
+ */
+class GameState {
+}
