ae1ad2c490e7439582c65557bb6870ec3b22bb07
WoLpH <Rick@Fawo.nl>
2013-04-01 01:33:07 +0200

    Added custom board to enable saving the state

--- src/main/java/org/jpacman/framework/model/UndoableBoard.java
+++ src/main/java/org/jpacman/framework/model/UndoableBoard.java
@@ -11,4 +11,21 @@ import java.util.HashMap;
  * 
  */
 public class UndoableBoard extends Board {
+
+	/**
+	 * Create a new board.
+	 * 
+	 * @param w
+	 *            Width of the board
+	 * @param h
+	 *            Height of the board
+	 */
+	public UndoableBoard(int w, int h) {
+		super(w, h);
+		this.sprites = new HashMap<SpriteType, ArrayList<IUndoableSprite>>();
+		for (SpriteType spriteType : SpriteType.values()) {
+			this.sprites.put(spriteType, new ArrayList<IUndoableSprite>());
+		}
+	}
+
 }
