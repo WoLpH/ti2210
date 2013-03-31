ac305c28725dead98c2382098eb7ad7bb6ca5575
WoLpH <Rick@Fawo.nl>
2013-04-01 01:33:07 +0200

    added undoable pacman as parameter to be able to communicate between the instances

--- src/main/java/org/jpacman/framework/factory/UndoableGameFactory.java
+++ src/main/java/org/jpacman/framework/factory/UndoableGameFactory.java
@@ -1,16 +1,38 @@
 package org.jpacman.framework.factory;
 
+import org.jpacman.framework.model.Board;
 import org.jpacman.framework.model.Game;
+import org.jpacman.framework.model.Ghost;
+import org.jpacman.framework.model.Player;
+import org.jpacman.framework.model.UndoableBoard;
+import org.jpacman.framework.model.UndoableFood;
 import org.jpacman.framework.model.UndoableGame;
+import org.jpacman.framework.model.UndoableGhost;
+import org.jpacman.framework.model.UndoablePlayer;
+import org.jpacman.framework.model.UndoableWall;
+import org.jpacman.framework.model.Wall;
+import org.jpacman.framework.ui.UndoablePacman;
 
 /**
  * Identical to {@link DefaultGameFactory} except that it returns an instance of
- * {@link UndoableGame} instead of a Game instance.
+ * {@link UndoableGame} instead of a Game instance and an instance of {@link UndoableBoard} instead
+ * of Board.
  * 
  * @author Rick van Hattem <Rick.van.Hattem@Fawo.nl>
  */
 public class UndoableGameFactory extends DefaultGameFactory {
 	private UndoableGame undoableGame;
+	private final UndoablePacman undoablePacman;
+
+	/**
+	 * Create a new UndoableGameFactory.
+	 * 
+	 * @param undoablePacman
+	 *            UndoablePacman link for callbacks from undo/redo buttons.
+	 */
+	public UndoableGameFactory(UndoablePacman undoablePacman) {
+		this.undoablePacman = undoablePacman;
+	}
 
 	@Override
 	protected Game getGame() {
