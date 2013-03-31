c3cfeb6f30fce5fc99c1e71d88369d6829ad5441
WoLpH <Rick@Fawo.nl>
2013-04-01 01:33:07 +0200

    Creating custom interaction for undo/redo actions

---
+++ src/main/java/org/jpacman/framework/ui/UndoablePacmanInteraction.java
@@ -0,0 +1,10 @@
+package org.jpacman.framework.ui;
+
+import org.jpacman.framework.model.UndoableGame;
+
+/**
+ * @author Rick van Hattem <Rick.van.Hattem@Fawo.nl>
+ * 
+ */
+class UndoablePacmanInteraction extends PacmanInteraction {
+}
