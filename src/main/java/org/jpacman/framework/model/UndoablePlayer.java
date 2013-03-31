cd4736993cba4fdb6eca37d85694386cdc608301
WoLpH <Rick@Fawo.nl>
2013-04-01 01:33:07 +0200

    Added sprite subclass to enable class-specific actions.

---
+++ src/main/java/org/jpacman/framework/model/UndoablePlayer.java
@@ -0,0 +1,8 @@
+package org.jpacman.framework.model;
+
+/**
+ * @author Rick van Hattem <Rick.van.Hattem@Fawo.nl>
+ * 
+ */
+public class UndoablePlayer extends Player implements IUndoableSprite {
+}
