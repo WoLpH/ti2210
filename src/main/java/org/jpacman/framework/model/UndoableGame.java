d0f49cfa2025f329b878fa77c272f2bfb3955f48
WoLpH <Rick@Fawo.nl>
2013-04-01 01:33:07 +0200

    added undoable game stub

---
+++ src/main/java/org/jpacman/framework/model/UndoableGame.java
@@ -0,0 +1,10 @@
+package org.jpacman.framework.model;
+
+/**
+ * Special version of the Game which implements and undo function.
+ * 
+ * @author Rick van Hattem <Rick.van.Hattem@Fawo.nl>
+ */
+public class UndoableGame extends Game {
+
+}
