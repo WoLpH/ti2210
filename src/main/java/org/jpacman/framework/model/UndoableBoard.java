17c7674233c96245be79a4a1946848b8d25b075b
WoLpH <Rick@Fawo.nl>
2013-04-01 01:33:07 +0200

    Added custom board to enable saving the state

---
+++ src/main/java/org/jpacman/framework/model/UndoableBoard.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package org.jpacman.framework.model;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * @author Rick van Hattem <Rick.van.Hattem@Fawo.nl>
+ * 
+ */
+public class UndoableBoard extends Board {
+}
