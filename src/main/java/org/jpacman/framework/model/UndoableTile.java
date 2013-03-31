01e31bab3d3563da36cead07381811e30901315e
WoLpH <Rick@Fawo.nl>
2013-04-01 01:33:07 +0200

    Make the tile comparable to skip useless replaces

--- src/main/java/org/jpacman/framework/model/UndoableTile.java
+++ src/main/java/org/jpacman/framework/model/UndoableTile.java
@@ -14,4 +14,18 @@ class UndoableTile extends Tile {
 		super(tile.getX(), tile.getY());
 	}
 
+	/**
+	 * @param obj
+	 *            The tile to compare to
+	 * @return True if the coordinates of the two tiles are identical
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof Tile) {
+			Tile tile = (Tile) obj;
+			return getX() == tile.getX() && getY() == tile.getY();
+		} else {
+			return false;
+		}
+	}
 }
