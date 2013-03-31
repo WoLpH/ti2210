b644e0dd1cbaf9db69dd3196350bbb9f039c880a
WoLpH <Rick@Fawo.nl>
2013-04-01 01:33:07 +0200

    Added tile clone method for saving the state

--- src/main/java/org/jpacman/framework/model/UndoablePlayer.java
+++ src/main/java/org/jpacman/framework/model/UndoablePlayer.java
@@ -6,12 +6,19 @@ package org.jpacman.framework.model;
  */
 public class UndoablePlayer extends Player implements IUndoableSprite {
 
+	private final UndoableSpriteMixin mixin;
+
 	/**
 	 * Create a new undoable player.
 	 */
 	public UndoablePlayer() {
 		super();
 		this.mixin = new UndoableSpriteMixin(this);
+	}
+
+	@Override
+	public UndoableTile cloneTile() {
+		return this.mixin.cloneTile();
 	}
 
 }
