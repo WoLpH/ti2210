0ed87d42263cd89aa29172c037281f06245b35c1
WoLpH <Rick@Fawo.nl>
2013-04-06 01:44:46 +0200

    added undoable pacman test for 100% coverage

---
+++ src/test/java/org/jpacman/test/framework/ui/UndoablePacmanTest.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package org.jpacman.test.framework.ui;
+
+import org.jpacman.framework.factory.FactoryException;
+import org.jpacman.framework.ui.UndoablePacman;
+import org.junit.Test;
+
+/**
+ * @author Rick van Hattem <Rick.van.Hattem@Fawo.nl>
+ * 
+ */
+public class UndoablePacmanTest {
+
+	/**
+	 * Test the main interface.
+	 * 
+	 * @throws FactoryException
+	 *             Shouldn't happen
+	 */
+	@Test
+	public void test() throws FactoryException {
+		UndoablePacman.main(null);
+	}
+
+}
