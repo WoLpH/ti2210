f84985c0fe303d34b603e0b189469cddce080f04
WoLpH <Rick@Fawo.nl>
2013-04-05 15:51:18 +0200

    added undo story test

--- src/test/java/org/jpacman/test/framework/accept/UndoStoryTest.java
+++ src/test/java/org/jpacman/test/framework/accept/UndoStoryTest.java
@@ -1,7 +1,10 @@
 package org.jpacman.test.framework.accept;
 
-import org.jpacman.framework.ui.MainUI;
+import org.jpacman.framework.ui.PacmanInteraction;
 import org.jpacman.framework.ui.UndoablePacman;
+import org.jpacman.framework.ui.UndoablePacmanInteraction;
+import org.junit.Assert;
+import org.junit.Test;
 
 /**
  * Story test for the undoable pacman.
@@ -12,13 +15,47 @@ public class UndoStoryTest extends MovePlayerStoryTest {
 	private UndoablePacman pacman;
 
 	@Override
-	protected MainUI getUI() {
+	protected UndoablePacmanInteraction getEngine() {
+		return (UndoablePacmanInteraction) super.getEngine();
+	}
+
+	@Override
+	protected UndoablePacman getUI() {
 		return this.pacman;
 	}
 
 	@Override
-	public MainUI makeUI() {
+	public UndoablePacman makeUI() {
 		this.pacman = new UndoablePacman();
 		return this.pacman;
 	}
+
+	/**
+	 * Test that a player is playing again after winning and undoing.
+	 */
+	@Test
+	public void test_S2_6_PlayerWinUndo() {
+		test_S2_5_PlayerWin();
+		getUI().getGame().undo();
+		getEngine().start();
+
+		/* Undoing the winning? */
+		Assert.assertEquals(PacmanInteraction.MatchState.PLAYING, getEngine()
+				.getCurrentState());
+	}
+
+	/**
+	 * Test that a player is playing again after winning and undoing.
+	 */
+	@Test
+	public void test_S2_7_PlayerLoseUndo() {
+		test_S2_4_PlayerDie();
+		getUI().getGame().undo();
+		getEngine().start();
+
+		/* Undoing the winning? */
+		Assert.assertEquals(PacmanInteraction.MatchState.PLAYING, getEngine()
+				.getCurrentState());
+	}
+
 }
